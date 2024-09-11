8783e7566bf3f43a3d8bbff9f15f3b459f2f067e
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-11 03:32:12 +0000

    beautify the version text

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 const version_placeholder = "phase.major.minor_patch";
+
 var versionElement = document.createElement('p');
+versionElement.id = 'version_text';
 versionElement.innerHTML = version_placeholder+'<br>';
 
 document.body.prepend(versionElement);
